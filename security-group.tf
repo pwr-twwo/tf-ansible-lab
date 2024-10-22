c898183383c2c0c03b1dae396db2a8f49d83da44
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2024-10-22 16:39:27 +0000

    feat: add security group

---
+++ security-group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "web_server" {
+  name = "web_server"
+
+  tags = {
+    Name = "Web Server SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.web_server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
