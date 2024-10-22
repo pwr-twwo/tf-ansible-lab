cf5187c021ff2d9984728ac89e07be38459c6f8a
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2024-10-22 22:14:52 +0000

    chore: ignore ansible/inventory.ini
    It is automatically created and does not have to be stored in git

--- security-group.tf
+++ security-group.tf
@@ -12,6 +12,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+  tags = {
+    Name = "Allow HTTP in"
+  }
+  
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
@@ -20,11 +24,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+  tags = {
+    Name = "Allow SSH in"
+  }
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
   security_group_id = aws_security_group.web_server.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
+  tags = {
+    Name = "Allow all trafic out"
+  }
+
 }
 
