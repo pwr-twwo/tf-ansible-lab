6ac951307d91f299a755e4e16eea9aa2f3318b28
Wojciech Thomas <wojciech.thomas@pwr.edu.pl>
2024-10-22 21:40:11 +0000

    feat: add ansible provisioner
    Ansible povisioner should be runned when remote instance is available. It is convenient to use remote-exec to check availability of the provisioned host.

--- host.tf
+++ host.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web_server" {
   instance_type = "t2.micro"
 
   security_groups = [aws_security_group.web_server.name]
-  
+
   key_name = "vockey"
 
   tags = {
@@ -11,10 +11,44 @@ resource "aws_instance" "web_server" {
   }
 }
 
+resource "local_file" "ip" {
+  content  = <<EOT
+  [webservers]
+  ${aws_instance.web_server.public_ip}
+  EOT
+  filename = "ansible/inventory.ini"
+}
+
+resource "terraform_data" "ssh_connection" {
+  provisioner "remote-exec" {
+    inline = [
+      "echo '$hostname connected...'"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "$HOME/.ssh/labsuser.pem"
+    host        = aws_instance.web_server.public_ip
+  }
+}
+
+resource "terraform_data" "ansible_provisioner" {
+  provisioner "local-exec" {
+    working_dir = "ansible/"
+    command     = "ansible-playbook web_server.yml"
+  }
+  depends_on = [
+    terraform_data.ssh_connection
+  ]
+}
+
 output "instance_ip_address" {
-    value = aws_instance.web_server.public_ip
+  value = aws_instance.web_server.public_ip
 }
 
 output "instance_dns_address" {
-    value = aws_instance.web_server.public_dns
+  value = aws_instance.web_server.public_dns
 }
+
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    type = string
-    default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "web_server_name" {
-    type = string
+  type = string
 }
 
